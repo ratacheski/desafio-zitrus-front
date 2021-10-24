6013d5e810d5089c984247dd6763d3b5245d5848
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-10-24 10:14:53 -0300

    Commit Inicial do Projeto

---
+++ components/TheAppBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TheAppBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model='drawer'
+      :mini-variant='miniVariant'
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for='(item, i) in items'
+          :key='i'
+          :to='item.to'
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text='item.title' />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <TheAppBar @toggleDrawer='drawer = !drawer'
+               @toggleMiniVariant='miniVariant = !miniVariant'
+               :miniVariant='miniVariant' />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <TheFooter />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-pencil',
+          title: 'Produtos',
+          to: '/produtos'
+        }
+      ],
+      miniVariant: false
+    }
+  }
+}
+</script>
