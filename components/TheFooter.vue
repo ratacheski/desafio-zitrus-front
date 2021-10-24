6556ec62017625bd87b801dd1d351b6398dccc51
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-10-24 17:13:02 -0300

    Ajustes do footer para adequação do tema

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,8 +2,21 @@
   <v-footer
     absolute
     app
+    dark
+    color='footer'
   >
-    <span>&copy; {{ new Date().getFullYear() }}</span>
+    <span class='font-weight-bold'>&copy; {{ new Date().getFullYear() }}</span>
+    <v-spacer />
+    <span>Desenvolvido por Rafael Ratacheski</span>
+    <v-spacer />
+    <div class='mx-2'>
+      <a href='https://github.com/ratacheski' class='text-decoration-none' target='_blank'>
+        <v-icon>mdi-github</v-icon>
+      </a>
+      <a href='https://www.linkedin.com/in/ratacheski/' target='_blank' class='text-decoration-none'>
+        <v-icon>mdi-linkedin</v-icon>
+      </a>
+    </div>
   </v-footer>
 </template>
 
